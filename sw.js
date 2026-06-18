81be57662c2e392b07ce1e246ff3c2ba5f2a336e
virtualvaultboy-pixel <virtual.vaultboy@gmail.com>
2026-06-18 12:28:29 +0200

    FitCircuit: PWA séance bodyweight avec chrono + vidéos de démo

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'fitcircuit-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // On ne met en cache que le shell de l'app (même origine). Les vidéos YouTube passent direct au réseau.
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
